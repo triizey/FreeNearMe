e3fa7a660a464f380efe6786c462d03d61f00a27
sunmengyue <mengyuesun@gmail.com>
2021-10-08 18:03:40 -0400

    add Jasveer's signin page and functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,134 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import { firebase } from './firebase';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App = () => {
   const [events, setEvents] = useState([]);
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
 
   useEffect(() => {
+    authListener();
     axios.get(`/events.json`).then((res) => setEvents(res.data.results));
   }, []);
+
+  const SignInWithGoogle = () => {
+    const google_provider = new firebase.auth.GoogleAuthProvider(); // creates a provider
+    firebase
+      .auth()
+      .signInWithPopup(google_provider)
+      .then((res) => {
+        console.log(res);
+        if (!res.additionalUserInfo.profile.email.includes('.edu')) {
+          setUser('');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const clearInputs = () => {
+    setEmail('');
+    setPassword('');
+  };
+  const clearErrors = () => {
+    setEmailError('');
+    setPasswordError('');
+  };
+
+  const handleLogin = () => {
+    if (email.includes('.edu')) {
+      clearErrors();
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch((err) => {
+          switch (err.code) {
+            case 'auth/invalid-email':
+            case 'auth/user-disabled':
+            case 'auth/user-not-found':
+              setEmailError(err.message);
+              break;
+            case 'auth/wrong-password':
+              setPasswordError(err.message);
+              break;
+            default:
+              setPasswordError(err.message);
+          }
+        });
+    } else {
+      setEmailError('email is not valid');
+    }
+  };
+
+  const handleSignup = () => {
+    if (email.includes('.edu')) {
+      clearErrors();
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .catch((err) => {
+          switch (err.code) {
+            case 'auth/email-already-in-use':
+            case 'auth/invalid-email':
+              setEmailError(err.message);
+              break;
+            case 'auth/weak-password':
+              setPasswordError(err.message);
+              break;
+            default:
+              setPasswordError(err.message);
+          }
+        });
+    } else {
+      setEmailError('email is not valid');
+    }
+  };
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+  };
+
+  const authListener = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // every time user logs in or signup then we have user so we want to clear the inputs and set the user
+        console.log(user);
+        if (!user.multiFactor.user.email.includes('.edu')) {
+          setUser('');
+          return;
+        }
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser('');
+      }
+    });
+  };
+
+  <Route exact path="/">
+    <Home handleLogout={handleLogout} events={events} />
+  </Route>;
 
   return (
     <Router>
       <Header />
       <div>
-        <Route path="/" exact render={() => <Home events={events} />} />
+        <Switch>
+          <Route exact path="/">
+            <Home handleLogout={handleLogout} events={events} />
+          </Route>
+        </Switch>
       </div>
       <Footer />
     </Router>
