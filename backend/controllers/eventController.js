46881cfdb275cc833f9ed464cc46b7b4931770b4
sunmengyue <mengyuesun@gmail.com>
2021-10-08 11:35:08 -0400

    add a controller folder

---
+++ backend/controllers/eventController.js
@@ -0,0 +1,26 @@
+const Event = require('../models/eventModel.js');
+const asyncHandler = require('express-async-handler');
+
+// @desc Fetch all events
+// @route GET /api/events
+// @access Public
+const getEvents = asyncHandler(async (req, res) => {
+  Event.find().then((data) => res.send(data));
+});
+
+// @desc Fetch a single event
+// @route GET /api/events/:id
+// @access Public
+const getSingleEvent = asyncHandler(async (req, res) => {
+  // find by req.params.id
+  const singleEvent = await Event.findOne({ uid: req.params.id });
+  if (singleEvent) {
+    res.status(200).send(singleEvent);
+  } else {
+    // formated but not existed id
+    res.status(404);
+    throw new Error('Event not found');
+  }
+});
+
+module.exports = { getEvents, getSingleEvent };
--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,33 +1,14 @@
 const router = require('express').Router();
-const Event = require('../models/eventModel.js');
-const asyncHandler = require('express-async-handler');
+const { getEvents, getSingleEvent } = require('../controllers/eventController');
 
 // @desc Fetch all events
 // @route GET /api/events
 // @access Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    Event.find().then((data) => res.send(data));
-  }),
-);
+router.route('/').get(getEvents);
 
 // @desc Fetch a single event
 // @route GET /api/events/:id
 // @access Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    // find by req.params.id
-    const singleEvent = await Event.findOne({ uid: req.params.id });
-    if (singleEvent) {
-      res.status(200).send(singleEvent);
-    } else {
-      // formated but not existed id
-      res.status(404);
-      throw new Error('Event not found');
-    }
-  }),
-);
+router.route('/:id').get(getSingleEvent);
 
 module.exports = router;
