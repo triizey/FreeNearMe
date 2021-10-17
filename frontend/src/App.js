f14deeb240fb24b14e8bf50b5de8b3dcfbc07391
James Jager <jviper882000@gmail.com>
2021-10-16 22:04:31 -0400

    quick update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import useGetUserID from "./customHooks/useGetUserID";
 import EventDetails from "./pages/EventDetails";
+import useGetUserEvents from "./customHooks/useGetUserEvents";
 
 const App = () => {
   const [defaultEvents, setDefaultEvents] = useState([]);
